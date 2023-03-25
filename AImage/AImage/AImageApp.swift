037caf453a93447289589bf98494b6bca0b4a9c7
Himanshu Vinchurkar <128510994+Himanshu-00@users.noreply.github.com>
2023-03-25 20:58:39 +0530

    Add files via upload

---
+++ AImage/AImage/AImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AImageApp.swift
+//  AImage
+//
+//  Created by Himanshu Vinchurkar on 21/03/23.
+//
+
+import SwiftUI
+
+@main
+struct AImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
